05b3e4a36338edcb0b6569afde67c24bf5bad9aa
Emre BOZKURT <e_bozkurt@anadolu.edu.tr>
2016-12-26 09:24:55 +0300

    Changed colors and visuals

--- Hasilat/app/src/main/java/noskurt/com/hasilat/MainActivity.java
+++ Hasilat/app/src/main/java/noskurt/com/hasilat/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_haberler) {
 
         } else if (id == R.id.nav_gallery) {
 
