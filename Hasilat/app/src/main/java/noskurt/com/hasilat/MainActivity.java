413fcbb9e233a82ac20106260e19147168e7d5dd
Emre BOZKURT <e_bozkurt@anadolu.edu.tr>
2016-12-27 11:35:20 +0300

    Fixed table and some designs

--- Hasilat/app/src/main/java/noskurt/com/hasilat/MainActivity.java
+++ Hasilat/app/src/main/java/noskurt/com/hasilat/MainActivity.java
@@ -3,14 +3,19 @@ package noskurt.com.hasilat;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import java.util.Arrays;
+import java.util.List;
 
 import noskurt.com.hasilat.news.NewsFragmentView;
 import noskurt.com.hasilat.weekly.WeeklyFragmentView;
@@ -57,8 +62,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_haftalik) {
             WeeklyFragmentView fragment = new WeeklyFragmentView();
             changeFragment(fragment);
+        } else if (id == R.id.nav_about) {
+            aboutUsAlert();
         }
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -70,5 +76,22 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_place, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void aboutUsAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("HAKKIMIZDA");
+        builder.setMessage(R.string.about_us);
+        builder.setCancelable(true);
+
+        builder.setNeutralButton("TAMAM", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
